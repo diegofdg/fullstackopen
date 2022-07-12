b33d5b90d4ace65f8a0b1aef1d1d468de43597a9
diegofdg <diegofdg@gmail.com>
2022-07-12 06:54:25 -0300

    ejercicio 1.2-paso 2

--- part_01/courseinfo/src/index.js
+++ part_01/courseinfo/src/index.js
@@ -9,12 +9,29 @@ const Header = (props) => {
     )
 }
 
+const Part = (props) => {
+    return (
+        <p>
+            {props.part} {props.exercises}
+        </p>
+    )
+}
+
 const Content = (props) => {
     return (
         <div>
-            <p>
-                {props.part} {props.exercises}
-            </p>            
+            <Part 
+                part={props.part1}
+                exercises={props.exercises1}
+            />
+            <Part 
+                part={props.part2}
+                exercises={props.exercises2}
+            />
+            <Part 
+                part={props.part3}
+                exercises={props.exercises3}
+            />        
         </div>
     )
 }
@@ -42,18 +59,15 @@ const App = () => {
                 course={course}
             />
             <Content 
-                part={part1} 
-                exercises={exercises1}
+                part1={part1} 
+                exercises1={exercises1}
+            
+                part2={part2} 
+                exercises2={exercises2}
+            
+                part3={part3} 
+                exercises3={exercises3}
             />
-            <Content 
-                part={part2} 
-                exercises={exercises2}
-            />
-            <Content 
-                part={part3} 
-                exercises={exercises3}
-            />
-
             <Total 
                 exercises1={exercises1}
                 exercises2={exercises2}
