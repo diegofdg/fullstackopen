41ec26d904a4dd9d5b7c2b2111f16607dc5b1443
diegofdg <diegofdg@gmail.com>
2022-07-13 19:12:23 -0300

    ejercicio 2.1-paso 6

---
+++ part_01/courseinfo/src/components/Content.js
@@ -0,0 +1,19 @@
+import Part from './Part';
+
+const Content = (props) => {
+    return (
+        <div>
+            {
+                props.parts.map((element) =>
+                    <Part
+                        key={element.id}
+                        name={element.name}
+                        exercises={element.exercises}
+                    />
+                )
+            }
+        </div>
+    );
+}
+
+export default Content;
--- part_01/courseinfo/src/index.js
+++ part_01/courseinfo/src/index.js
@@ -1,82 +1,36 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
-const Header = ({ course }) => {
-    return (
-        <h1>
-            {course.name}
-        </h1>
-    );
-}
-
-const Part = ({ part, exercises }) => {
-    return (
-        <p>
-            {part} {exercises}
-        </p>
-    );
-}
-
-const Content = ({ parts }) => {
-    return (
-        <div>
-            <Part 
-                part={parts[0].name}
-                exercises={parts[0].exercises}
-            />
-            <Part 
-                part={parts[1].name}
-                exercises={parts[1].exercises}
-            />
-            <Part 
-                part={parts[2].name}
-                exercises={parts[2].exercises}
-            />        
-        </div>
-    );
-}
-
-const Total = ({ parts }) => {
-  return (
-      <p>          
-          Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-      </p>
-  );
-}
+import Course from './components/Course';
 
 const App = () => {
     const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+      ],
     }
-
+  
     return (
-        <div>
-            <Header 
-                course={course}
-            />
-            <Content 
-                parts={course.parts}
-            />
-            <Total 
-                parts={course.parts}
-            />
-        </div>
+        <Course
+            course={course}
+        />
     );
-}
+  }
 
 const container = document.getElementById('root');
 const root = createRoot(container);
