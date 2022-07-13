c7037a60fb5c2e6e6b7f2f0e42eeb71d3fa0400f
diegofdg <diegofdg@gmail.com>
2022-07-13 19:42:37 -0300

    ejercicio 2.4-paso 9 y ejercicio 2.5-modulo separado

--- part_01/courseinfo/src/index.js
+++ part_01/courseinfo/src/index.js
@@ -3,32 +3,63 @@ import { createRoot } from 'react-dom/client';
 import Course from './components/Course';
 
 const App = () => {
-    const course = {
-      id: 1,
-      name: 'Half Stack application development',
-      parts: [
+    const courses = [
         {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1,
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1,
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2,
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3,
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4,
+                },
+            ],
         },
         {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2,
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1,
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2,
+                },
+            ],
         },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3,
-        },
-      ],
-    }
+    ]
   
     return (
-        <Course
-            course={course}
-        />
+        <div>
+            {
+                courses.map(
+                    (element) =>
+                        <Course
+                            key={element.id}
+                            course={element}
+                        />
+                )
+            }
+        </div>
     );
   }
 
