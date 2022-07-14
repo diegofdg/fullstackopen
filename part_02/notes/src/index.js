74537b06375cd7866d00991f496c0bf7a94ed834
diegofdg <diegofdg@gmail.com>
2022-07-14 16:24:44 -0300

    parte 2-punto c-effect-hooks

--- part_02/notes/src/index.js
+++ part_02/notes/src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client';
-import axios from 'axios';
 
-axios
-    .get('http://localhost:3001/notes')
-    .then(response => {
-        const notes = response.data;    
-        const container = document.getElementById('root');
-        const root = createRoot(container);
-        root.render(<App notes={notes} />);
-    });
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
