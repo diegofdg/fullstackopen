8debe91fe2587d6a749c2643968a4f376acebf15
diegofdg <diegofdg@gmail.com>
2022-07-14 16:17:26 -0300

    parte 2-punto c-axios y promesas

--- part_02/notes/src/index.js
+++ part_02/notes/src/index.js
@@ -1,30 +1,13 @@
 import React from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client';
+import axios from 'axios';
 
-const notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        date: '2019-05-30T17:30:31.098Z',
-        important: true
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only JavaScript',
-        date: '2019-05-30T18:39:34.091Z',
-        important: false
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        date: '2019-05-30T19:20:14.298Z',
-        important: true
-    }
-]
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App notes={notes} />);
-
-//ReactDOM.render(<App notes={notes} />, document.getElementById('root'));
+axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+        const notes = response.data;    
+        const container = document.getElementById('root');
+        const root = createRoot(container);
+        root.render(<App notes={notes} />);
+    });
