afa4dd2364721ca3072639be593d0f321a2af02f
diegofdg <diegofdg@gmail.com>
2022-07-14 20:54:34 -0300

    parte 2-punto e-mensaje de error mejorado

--- part_02/notes/src/App.js
+++ part_02/notes/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Note from './components/Note';
 import noteService from './services/notes';
+import Notification from './components/Notification';
 
 const App = () => {
     const [notes, setNotes] = useState([]);
     const [newNote, setNewNote] = useState('');
     const [showAll, setShowAll] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
 
     useEffect(() => {
         noteService
@@ -26,6 +28,7 @@ const App = () => {
             .create(noteObject)
             .then(returnedNote => {
                 setNotes(notes.concat(returnedNote));
+                setNewNote('');
             });
     }
 
@@ -53,7 +56,10 @@ const App = () => {
             })
             .catch(error => {
                 console.log(error);
-                alert(`the note '${note.content}' was already deleted from server`);
+                setErrorMessage(`Note '${note.content}' was already removed from server`);
+                setTimeout(() => {
+                    setErrorMessage(null);
+                }, 5000);
                 setNotes(notes.filter(n => n.id !== id));
             });
     }
@@ -61,6 +67,9 @@ const App = () => {
     return (
         <div>
             <h1>Notes</h1>
+            <Notification
+                message={errorMessage}
+            />
             <div>
                 <button
                     onClick={() => setShowAll(!showAll)}
