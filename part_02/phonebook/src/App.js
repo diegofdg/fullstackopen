2b1759a75d837c1976c37c3bf82dc17ccac553fa
diegofdg <diegofdg@gmail.com>
2022-07-14 07:54:02 -0300

    ejercicio 2.8-paso 3 y ejercicio 2.9-paso 4

--- part_02/phonebook/src/App.js
+++ part_02/phonebook/src/App.js
@@ -4,12 +4,28 @@ const App = () => {
     const [ persons, setPersons ] = useState(
         [
             { 
-                name: 'Arto Hellas' 
+                name: 'Arto Hellas',
+                number: '040-123456'
+            },
+            {
+                name: 'Ada Lovelace',
+                number: '39-44-5323523'
+            },
+            {
+                name: 'Dan Abramov',
+                number: '12-43-234345'
+            },
+            {
+                name: 'Mary Poppendieck',
+                number: '39-23-6423122'
             }
         ]
     );
 
+    const [ filteredPersons, setFilteredPersons ] = useState([]);
     const [ newName, setNewName ] = useState('');
+    const [ newNumber, setNewNumber ] = useState('');
+    const [ newFilter, setNewFilter ] = useState('');
 
     const addPerson = (e) => {
         e.preventDefault();
@@ -20,31 +36,60 @@ const App = () => {
 
         if(person.length === 0) {
             const newPerson = {
-                name: newName
+                name: newName,
+                number: newNumber
             }
             setPersons(persons.concat(newPerson));
             setNewName('');
+            setNewNumber('');
         } else {
             alert(`${newName} is already added to the phonebook`);
         }
     }
 
+    const handleChangeFilter = (e) => {
+        console.log(filteredPersons);
+        setNewFilter(e.target.value);
+        const regex = new RegExp( newFilter, 'i' );
+        const filter = () => persons.filter(person => person.name.match(regex));
+        setFilteredPersons(filter);
+    }   
+
     const handleChangeName = (e) => {     
         setNewName(e.target.value);    
+    }
+
+    const handleChangeNumber = (e) => {
+        setNewNumber(e.target.value);
     }
 
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                filter shown with 
+                <input 
+                    value={newFilter} 
+                    onChange={handleChangeFilter} 
+                />
+            </div>
             <form
                 onSubmit={addPerson}
             >
+                <h2>add a new</h2>
                 <div>
                     name:
                     <input
                         value={newName} 
                         onChange={handleChangeName}
                     />
+                </div>
+                <div>
+                    number: 
+                        <input
+                            value={newNumber} 
+                            onChange={handleChangeNumber}
+                        />
                 </div>
                 <div>
                     <button 
@@ -56,13 +101,28 @@ const App = () => {
             </form>
             <h2>Numbers</h2>
             <div>
-                {persons.map(person => (
-                    <p
-                        key={person.name}
-                    >
-                        {person.name}                
-                    </p> 
-                ))}
+                {filteredPersons.length > 0 
+                    ?
+                        <div>
+                            {filteredPersons.map(person => (
+                                <p
+                                    key={person.name}
+                                >
+                                    {person.name} {person.number}                
+                                </p>
+                            ))}
+                        </div>
+                    : 
+                        <div>
+                            {persons.map(person => (
+                                <p
+                                    key={person.name}
+                                >
+                                    {person.name}
+                                </p>
+                            ))}
+                        </div>
+                    }
             </div>
         </div>
     );
