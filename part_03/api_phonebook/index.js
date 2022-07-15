a0875e2adf405c69acd964692d9605908bc76fc0
diegofdg <diegofdg@gmail.com>
2022-07-15 18:55:00 -0300

    ejercicio 3.2-paso 2

--- part_03/api_phonebook/index.js
+++ part_03/api_phonebook/index.js
@@ -29,6 +29,17 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>');
 });
 
+app.get('/info', (request, response) => {
+    response.send(
+            `<div>
+        <p>Phonebook has info for ${persons.length} people</p>
+    </div>
+    <div>
+        <p>${new Date()}</p>
+    </div>`
+    );
+});
+
 app.get('/api/persons', (request, response) => {
     response.json(persons);
 });
