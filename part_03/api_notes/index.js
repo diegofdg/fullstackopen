ebe34dd420035c37dcbe180077fa89b7e0251432
diegofdg <diegofdg@gmail.com>
2022-07-15 15:47:38 -0300

    parte 3-punto a-recibiendo información

--- part_03/api_notes/index.js
+++ part_03/api_notes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 let notes = [  
     {    
@@ -40,6 +41,34 @@ app.get('/api/notes/:id', (request, response) => {
   
 app.get('/api/notes', (request, response) => {
     response.json(notes);
+});
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1;
+};
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body;
+
+    if (!body.content) {
+        return response.status(400).json({ 
+            error: 'content missing' 
+        });
+    };
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    };
+
+    notes = notes.concat(note);
+
+    response.json(note);
 });
 
 app.delete('/api/notes/:id', (request, response) => {
