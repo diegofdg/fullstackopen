027d56d408bec980b8eaabbce678a49f586db7a7
diegofdg <diegofdg@gmail.com>
2022-07-19 01:24:19 -0300

    parte 3-punto c-configurando la base de datos en su propio módulo

--- part_03/api_notes/index.js
+++ part_03/api_notes/index.js
@@ -1,35 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-const url = process.env.MONGODB_URI;
-
-mongoose.connect(url, 
-    {
-        useNewUrlParser: true, 
-        useUnifiedTopology: true
-    }
-);
-
-const noteSchema = new mongoose.Schema(
-    {
-        content: String,
-        date: Date,
-        important: Boolean,
-    }
-);
-
-noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-});
-
-const Note = mongoose.model('Note', noteSchema);
+const Note = require('./models/Note');
 
 const app = express();
 
