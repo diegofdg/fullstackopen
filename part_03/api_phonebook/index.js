cded20d6eacf7bd3d87ad3cee2937b0cae56d96b
diegofdg <diegofdg@gmail.com>
2022-07-19 08:41:38 -0300

    ejercicio 3.18-base de datos de la agenda telefónica-paso 6

--- part_03/api_phonebook/index.js
+++ part_03/api_phonebook/index.js
@@ -47,37 +47,50 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>');
 });
 
-app.get('/info', async (request, response) => {
-    const persons = await Person.find({});
-        
-    response.send(
-        `<div>
-            <p>Phonebook has info for ${persons.length} people</p>
-        </div>
-        <div>
-            <p>${new Date()}</p>
-        </div>`
-    );
+app.get('/info', (request, response, next) => {
+    Person
+        .find({})
+        .then(person => {
+            response.send(
+                `<div>
+                    <p>Phonebook has info for ${person.length} people</p>
+                </div>
+                <div>
+                    <p>${new Date()}</p>
+                </div>`
+        )    
+        })
+        .catch(error => {
+            next(error);
+        });
 });
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person
         .find({})
         .then(person => {
             response.json(person);
         })
+        .catch(error => {
+            next(error);
+        });
 });
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(p => {
-        return p.id === id;
-    });
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+	Person
+        .findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person);
+            } else {
+                response.status(404).send({
+                    error: 'not found'
+                })
+            }
+        })
+        .catch(error => {
+            next(error);
+        });
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -106,7 +119,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
             response.status(204).end();
         })
         .catch(error => {
-            console.log(error);
+            next(error);
         });
 });
 
