4af121009b0cff3e76f13a0032a4f9c0f6cc0a7c
diegofdg <diegofdg@gmail.com>
2022-07-19 11:12:44 -0300

    parte 3-punto d-lint

--- part_03/api_notes/models/Note.js
+++ part_03/api_notes/models/Note.js
@@ -1,44 +1,45 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 const mongoose = require('mongoose');
 
 const url = process.env.MONGODB_URI;
 
 mongoose
-    .connect(url, 
-        {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true
-        }
-    )
-    .then(result => {
-        console.log('connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-      })
+	.connect(url,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		}
+	)
+	.then(result => {
+		console.log('connected to MongoDB');
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message);
+	});
 
 const noteSchema = new mongoose.Schema(
-    {
-        content: 
+	{
+		content:
             {
-                type: String,
-                minlength: 5,
-                required: true
+            	type: String,
+            	minlength: 5,
+            	required: true
             },
-        date:
+		date:
             {
-                type: Date,
-                required: true
+            	type: Date,
+            	required: true
             },
-        important: Boolean,
-    }
+		important: Boolean,
+	}
 );
 
 noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	}
 });
 
 module.exports = mongoose.model('Note', noteSchema);
