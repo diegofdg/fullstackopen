0ecaa8adb79e02e2b4d1af933b680e51b135cbac
diegofdg <diegofdg@gmail.com>
2022-08-27 20:58:04 -0300

    ejercicio 4.19-expansiones de la lista de blogs-paso 7

--- part_04/api_bloglist/controllers/blogs.js
+++ part_04/api_bloglist/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/Blog');
 const User = require('../models/User');
+
+const getTokenFrom = request => {
+	const authorization = request.get('authorization');
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		return authorization.substring(7);
+	}
+	return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
@@ -9,6 +18,13 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body;
+
+  const token = getTokenFrom(request);
+	const decodedToken = jwt.verify(token, process.env.SECRET);
+	if (!token || !decodedToken.id) {
+		return response.status(401).json({ error: 'token missing or invalid' });
+	}
+	const user = await User.findById(decodedToken.id);
 
   if (!body.likes) {
     body.likes = 0;
@@ -20,22 +36,19 @@ blogsRouter.post('/', async (request, response, next) => {
     });
   }
 
-  const users = await User.find({});
-    const randomUser = users[Math.floor(Math.random() * users.length)];
-  
   const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes,
-      user: randomUser._id
+      user: user._id
   });
 
   try {
-    const result = await blog.save();
-    randomUser.blogs = randomUser.blogs.concat(blog._id);
-        await randomUser.save();
-    response.status(201).json(result);
+      const result = await blog.save();
+      user.blogs = user.blogs.concat(blog._id);
+      await user.save();
+      response.status(201).json(result);
   } catch (error) {
       if (error.code === 11000) {
 			  return response.status(400).json({ error: error.message });
