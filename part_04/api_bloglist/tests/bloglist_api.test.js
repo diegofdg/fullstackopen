bdcb8bb0143c7fe41c13882abf9c993d360d7f7f
diegofdg <diegofdg@gmail.com>
2022-08-27 00:02:03 -0300

    ejercicio 4.10-pruebas de lista de blogs-paso 3

--- part_04/api_bloglist/tests/bloglist_api.test.js
+++ part_04/api_bloglist/tests/bloglist_api.test.js
@@ -29,6 +29,30 @@ test('the unique identifier property of the blog posts is by default _id', async
 	expect(blogs[0]._id).toBeDefined();
 });
 
+test('a valid blog can be added ', async () => {
+	const newBlog = {
+		title: 'Canonical string reduction',
+		author: 'Edsger W. Dijkstra',
+		url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+		likes: 12
+	};
+
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(201)		
+		.expect('Content-Type', /application\/json/);
+
+	const blogsAtEnd = await helper.blogsInDb();
+	expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+	const contents = blogsAtEnd.map(n => n.title);
+	expect(contents).toContain(
+		'Canonical string reduction'
+	);
+});
+
+
 afterAll(() => {
 	mongoose.connection.close();
 });
