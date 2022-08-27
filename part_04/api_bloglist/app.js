738828169acd105bd7a639d943b194a2ac2a1885
diegofdg <diegofdg@gmail.com>
2022-08-26 23:57:26 -0300

    ejercicio 4.8-pruebas de lista de blogs-paso 1

--- part_04/api_bloglist/app.js
+++ part_04/api_bloglist/app.js
@@ -1,7 +1,26 @@
+const config = require('./utils/config');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
+
+const url = process.env.MONGODB_URI;
+
+console.log('connecting to', url);
+
+mongoose
+	.connect(url,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		})
+	.then(result => {
+		console.log('connected to MongoDB');
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message);
+	});
 
 app.use(cors());
 
