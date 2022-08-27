5c1745404805ccfc649658952c2f0ca563984b94
diegofdg <diegofdg@gmail.com>
2022-08-27 20:51:56 -0300

    ejercicio 4.18-expansiones de la lista de blogs-paso 6

--- part_04/api_bloglist/app.js
+++ part_04/api_bloglist/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 const url = config.MONGODB_URI;
 
@@ -29,5 +30,6 @@ app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 module.exports = app;
