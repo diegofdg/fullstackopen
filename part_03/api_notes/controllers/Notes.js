c8abac842064bb7e4a206bf10bf724cf600782d0
diegofdg <diegofdg@gmail.com>
2022-08-27 01:33:54 -0300

    parte 4-punto d-notas finales

--- part_03/api_notes/controllers/Notes.js
+++ part_03/api_notes/controllers/Notes.js
@@ -46,7 +46,7 @@ notesRouter.post('/', async (request, response) => {
 	const savedNote = await note.save();
 	user.notes = user.notes.concat(savedNote._id);
 	await user.save();
-	response.json(savedNote);
+	response.status(201).json(savedNote);
 });
 
 notesRouter.delete('/:id', async (request, response) => {
