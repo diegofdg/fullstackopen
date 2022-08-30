b47a16f040b63aed2fd3bb9c1f8ec57c11b8cf72
diegofdg <diegofdg@gmail.com>
2022-08-30 18:32:08 -0300

    ejercicio 5.2-frontend de la lista de blogs-paso 2

--- part_05/bloglist/src/App.js
+++ part_05/bloglist/src/App.js
@@ -17,12 +17,25 @@ const App = () => {
         )
     }, []);
 
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON);
+            setUser(user);
+            blogService.setToken(user.token);
+        }
+    }, []);
+
     const handleLogin = async(e) => {
         e.preventDefault();
         try {
             const user = await loginService.login({
                 username, password
             });
+            window.localStorage.setItem(
+                'loggedBlogappUser', JSON.stringify(user)
+            )
+            blogService.setToken(user.token);
             setUser(user);
             setUsername('');
             setPassword('');
@@ -33,6 +46,13 @@ const App = () => {
             }, 5000);
         }
     }
+
+    const handleLogout = async (e) => {
+        e.preventDefault();
+        window.localStorage.removeItem('loggedBlogappUser');
+        setUser(null);
+    }
+
     if (user === null) {
         return (
             <div>
@@ -71,7 +91,14 @@ const App = () => {
     return (
         <div>
             <h2>blogs</h2>
-            <p>{user.name} logged-in</p>
+            <p>
+                {user.name} logged-in
+                    <button
+                        onClick={handleLogout}
+                        type="submit"
+                    >logout
+                    </button>            
+            </p>
             {blogs.map(blog =>
                 <Blog key={blog.id} blog={blog} />
             )}
