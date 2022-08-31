7b7ffa23af95a31fe3faf83482aab195444191ff
diegofdg <diegofdg@gmail.com>
2022-08-31 20:22:23 -0300

    ejercicio 5.7-frontend de la lista de blogs-paso 7

--- part_05/bloglist/src/components/Blog.js
+++ part_05/bloglist/src/components/Blog.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Blog = ({ blog }) => {
+const Blog = ( props ) => {
+
+    const blog = props.blog;
+    const [visible, setVisible] = useState(false);
+    const showWhenVisible = { display: visible ? '' : 'none' };
+
+    const toggleVisibility = () => {
+        setVisible(!visible);
+  }
+
+    const buttonLabel = visible ? 'hide' : 'view';
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
     return (
-        <li className='blog'>
-            {blog.title} - {blog.author}
-        </li>
+        <div
+            style={blogStyle}
+        >
+            <div>
+                <p>
+                {blog.title} - {blog.author}
+                <button
+                        onClick={toggleVisibility}
+                    >{buttonLabel}
+                    </button>
+                </p>
+            </div>
+            <div
+                style={showWhenVisible}
+            >
+                <p>{blog.url}</p>
+                <p>
+                    likes {props.blog.likes}
+                    <button>like</button>
+                </p>                
+            </div>
+        </div>
     );
 }
 
