6cd4a243e22baed8893b76bcd4b321254465de80
diegofdg <diegofdg@gmail.com>
2022-09-01 20:40:40 -0300

    parte 5-punto d-omitiendo la interfaz de usuario

--- part_02/notes/cypress/integration/note_app.spec.js
+++ part_02/notes/cypress/integration/note_app.spec.js
@@ -11,16 +11,23 @@ describe('Note app', function() {
 	});
 
 	it('front page can be opened', function() {
-		cy.visit('http://localhost:3000');
 		cy.contains('Notes');
 		cy.contains('Note app, Department of Computer Science, University of Helsinki 2022');
 	});
 
-	it('login form can be opened', function() {
+	it('login fails with wrong password', function() {
 		cy.contains('log in').click();
+		cy.get('#username').type('root');
+		cy.get('#password').type('wrong');
+		cy.get('#login-button').click();
+		cy.get('.error')
+			.should('contain', 'wrong credentials')
+			.and('have.css', 'color', 'rgb(255, 0, 0)')
+			.and('have.css', 'border-bottom-style', 'solid');
+		cy.get('html').should('not.contain', 'Superuser Notes logged-in');
 	});
 
-	it('user can log in', function() {
+	it('login form can be opened', function() {
 		cy.contains('log in').click();
 		cy.get('#username').type('root');
 		cy.get('#password').type('root');
@@ -30,17 +37,31 @@ describe('Note app', function() {
 
 	describe('when logged in', function() {
 		beforeEach(function() {
-			cy.contains('log in').click();
-			cy.get('#username').type('root');
-			cy.get('#password').type('root');
-			cy.get('#login-button').click();
+			cy.login({ username: 'root', password: 'root' });
 		});
 
 		it('a new note can be created', function() {
 			cy.contains('new note').click();
-			cy.get('#textNote').type('a note created by cypress');
+			cy.get('input').type('a note created by cypress');
 			cy.contains('save').click();
 			cy.contains('a note created by cypress');
+		});
+
+		describe('and a note exists', function () {
+			beforeEach(function () {
+				cy.createNote({
+					content: 'another note cypress',
+					important: false
+				});
+			});
+
+			it('it can be made important', function () {
+				cy.contains('another note cypress')
+					.contains('make important')
+					.click();
+				cy.contains('another note cypress')
+					.contains('make not important');
+			});
 		});
 	});
 });
