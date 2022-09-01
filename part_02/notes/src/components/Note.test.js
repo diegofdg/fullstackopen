84e8f207bfed1602a9db37ab44cfd86b695184df
diegofdg <diegofdg@gmail.com>
2022-09-01 06:22:19 -0300

    parte 5-punto c-hacer click en los botones de las pruebas

--- part_02/notes/src/components/Note.test.js
+++ part_02/notes/src/components/Note.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { prettyDOM } from '@testing-library/dom';
 import Note from './Note';
 
@@ -16,7 +16,7 @@ test('renders content', () => {
 
 	const li = component.container.querySelector('li');
 
-	console.log(prettyDOM(li));
+	//console.log(prettyDOM(li));
 
 	component.debug();
 
@@ -36,4 +36,18 @@ test('renders content', () => {
 	expect(div).toHaveTextContent(
 		'Component testing is done with react-testing-library'
 	);
+});
+
+test('clicking the button calls event handler once', () => {
+	const note = {
+		content: 'Component testing is done with react-testing-library',
+		important: true
+	};
+	const mockHandler = jest.fn();
+	const component = render(
+		<Note note={note} toggleImportance={mockHandler} />
+	);
+	const button = component.getByText('make not important');
+	fireEvent.click(button);
+	expect(mockHandler.mock.calls).toHaveLength(1);
 });
