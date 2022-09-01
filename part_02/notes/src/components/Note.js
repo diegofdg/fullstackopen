f6a0458aee2bb7170c44be236d90641bd8750818
diegofdg <diegofdg@gmail.com>
2022-09-01 05:58:08 -0300

    parte 5-punto c-renderizando el componente para pruebas

--- part_02/notes/src/components/Note.js
+++ part_02/notes/src/components/Note.js
@@ -5,7 +5,9 @@ const Note = ({ note, toggleImportance }) => {
 		? 'make not important'
 		: 'make important';
 	return (
-		<li className='note'>
+		<li
+			className='note'
+		>
 			{note.content}
 			<button
 				onClick={toggleImportance}
