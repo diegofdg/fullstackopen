866c2755919c95d71420b784ac63c7f57dcd4dcf
diegofdg <diegofdg@gmail.com>
2022-09-01 18:09:38 -0300

    ejercicio 5.14-pruebas de listas de blogs-paso 2

--- part_05/bloglist/src/components/Blog.test.js
+++ part_05/bloglist/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
 
 describe('Blog component tests', () => {
@@ -19,6 +19,20 @@ describe('Blog component tests', () => {
 		);
 		expect(component.container).toHaveTextContent(
 			'React patterns - Michael Chan'
+		);
+	});
+
+	test('clicking the view button displays url and number of likes', () => {
+		const component = render(
+			<Blog blog={blog} updateBlog={mockUpdateBlog} deleteBlog={mockDeleteBlog} />
+		);
+		const button = component.getByText('view');
+		fireEvent.click(button);
+		expect(component.container).toHaveTextContent(
+			'https://reactpatterns.com/'
+		);
+		expect(component.container).toHaveTextContent(
+			'7'
 		);
 	});
 });
