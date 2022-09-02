5732c8fd5402ebd90759a46d341cd598c7d14ee4
diegofdg <diegofdg@gmail.com>
2022-09-01 21:12:08 -0300

    ejercicio 5.18-pruebas de extremo a extremo de la lista de blogs-paso 2

--- part_05/bloglist/cypress/integration/blog_app.spec.js
+++ part_05/bloglist/cypress/integration/blog_app.spec.js
@@ -1,12 +1,41 @@
 describe('Blog app', function() {
 	beforeEach(function() {
 		cy.request('POST', 'http://localhost:3001/api/testing/reset');
+		const user = {
+			name: 'Superuser Blog',
+			username: 'root',
+			password: 'root'
+		};
+		cy.request('POST', 'http://localhost:3001/api/users/', user);
 		cy.visit('http://localhost:3000');
 	});
 
 	it('Login form is shown', function() {
 		cy.contains('username');
 		cy.contains('password');
-		cy.contains('login');
+		cy.contains('log in');
+	});
+
+	describe('Login',function() {
+		it('succeeds with correct credentials', function() {
+			cy.contains('log in').click();
+			cy.get('#username').type('root');
+			cy.get('#password').type('root');
+			cy.get('#login-button').click();
+			cy.contains('Superuser Blog logged-in');
+		});
+
+		it('login fails with wrong password', function() {
+			cy.contains('log in').click();
+			cy.get('#username').type('root');
+			cy.get('#password').type('wrong');
+			cy.get('#login-button').click();
+
+			cy.get('.error')
+				.should('contain', 'wrong username or password')
+				.and('have.css', 'color', 'rgb(255, 0, 0)')
+				.and('have.css', 'border-bottom-style', 'solid');
+			cy.get('html').should('not.contain', 'Superuser Blog logged-in');
+		});
 	});
 });
